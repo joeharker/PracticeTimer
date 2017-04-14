44640f981796b971de6f4c39e819f1bbdca81fee
chipotlephonegap <jharker@chipotle.com>
2017-04-14 13:46:39 -0600

    ios styles

--- pages/init/init.js
+++ pages/init/init.js
@@ -1,5 +1,13 @@
 ﻿(function (donate) {
 	donate.setDonation('stripe', 1.00);
+
+	if (device && device.platform === 'iOS') {
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.href = 'pages/init/ios.css';
+		document.getElementsByTagName('head')[0].appendChild(link);
+	}
+
 	if (window.plugins && window.plugins.insomnia){
 		window.plugins.insomnia.keepAwake();
 	}
