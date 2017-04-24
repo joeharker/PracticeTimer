0334f344ce1af456489e90d064eabfa3a4c2d21a
joeharker <joeharker@gmail.com>
2017-04-23 20:29:58 -0600

    sound test 4 ios

--- services/configService.js
+++ services/configService.js
@@ -15,7 +15,7 @@ var configService = (function () {
 	function init() {
 		//PROD values as default
 		values = {
-			root: location.href.replace(/[^/]*$|[^/]+#.*$/, ''),
+			root: '',	//location.href.replace(/[^/]*$|[^/]+#.*$/, ''),
 			debug: false,
 			testable: values.testable
 		};
