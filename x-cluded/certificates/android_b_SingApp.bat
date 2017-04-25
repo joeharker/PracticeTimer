665f842927f19fc2286f4a00a8d0575816bc3342
joeharker <joeharker@gmail.com>
2017-04-24 18:40:53 -0600

    completed the android signing batch files

--- x-cluded/certificates/android_b_SingApp.bat
+++ x-cluded/certificates/android_b_SingApp.bat
@@ -1,14 +1,14 @@
 @echo off
-echo build an .apk with the keystore from the last batch cirt then rename the .apk and copy it to "Android\App-release.apk"
+echo build an .apk with the keystore from the last batch cirt then rename the .apk and copy it to "android\App-temp.apk"
 
 echo locating
 cd "%~dp0"
 
 echo signing
-"C:\Program Files\Java\jdk1.7.0_79\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore Android\App.keystore Android\App-release.apk App
+"c:\Program Files\Java\jdk1.8.0_131\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore Android\App.keystore Android\App-temp.apk App
 
 echo aligning
-zipalign\zipalign -v 4 Android\App-release.apk Android\App.apk
+"zipalign\zipalign.exe" -v 4 Android\App-temp.apk Android\App.apk
 
 pause
 cls
